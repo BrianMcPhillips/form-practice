074f613ef3ed7223bb255658270ff3f831d58d28
Brian McPhillips <B.McPhillips89@gmail.com>
2021-01-25 18:26:49 -0800

    fixed email validation bug

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ function showSuccess(input) {
 
 //Valid Email
 function isValidEmail(email) {
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase()); 
 }
 
 //Event Listener
@@ -41,7 +41,7 @@ if (username.value === '') {
 }
 if (email.value === '') {
   showError(email, 'Email is Required')
-} else if (!isValidEmail(email)) {
+} else if (!isValidEmail(email.value)) {
   showError(email, 'Please enter a valid Email')
 } else {
   showSuccess(email);
