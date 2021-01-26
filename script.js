f181d17b7fca93a4b2521b7fcc28fd4c661f7a48
Brian McPhillips <B.McPhillips89@gmail.com>
2021-01-25 16:36:14 -0800

    adds error and success color handling for blank and filled form.

--- script.js
+++ script.js
@@ -6,11 +6,19 @@ const password = document.getElementById('password');
 const confPass = document.getElementById('password-2');
 
 //Functions
+
+//Error input handling
 function showError(input, message) {
   const formControl = input.parentElement;
   formControl.className = 'form-control error';
   const small = formControl.querySelector('small');
   small.innerText = message;
+}
+
+//Success input handling
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
 }
 
 //Event Listener
@@ -24,5 +32,20 @@ if (username.value === '') {
   showError(username, 'Username is Required')
 } else {
   showSuccess(username);
+}
+if (email.value === '') {
+  showError(email, 'Email is Required')
+} else {
+  showSuccess(email);
+}
+if (password.value === '') {
+  showError(password, 'Password is Required')
+} else {
+  showSuccess(password);
+}
+if (confPass.value === '') {
+  showError(confPass, 'Please Re-enter your password')
+} else {
+  showSuccess(confPass);
 }
 });
